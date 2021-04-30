32b6aad0f909735132453a7843722e51560672bd
Andreas Wolf <andreas.wolf@diva-e.com>
2021-04-30 02:58:08 +0200

    First build

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://192.168.1.119:8080"
+export const BASE_URL = "http://localhost:8080"
 export const PLAYER_URL = `${BASE_URL}/player/current`
 export const PLAYER_RESUME_URL = `${BASE_URL}/player/resume`
 export const PLAYER_PAUSE_URL = `${BASE_URL}/player/pause`
